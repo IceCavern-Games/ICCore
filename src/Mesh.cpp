f8125aebe963ba2fc738d082490a439252beea6a
Jordan Hill <jordan.hill@comcast.net>
2025-07-07 23:05:58 -0700

    Add mesh to ICCore

---
+++ src/Mesh.cpp
@@ -0,0 +1,16 @@
+﻿#include <Mesh.h>
+
+namespace ICCore {
+    Mesh::Mesh() {
+
+    }
+
+    void Mesh::SetIndices(const std::vector<uint32_t> &indices) {
+        _indices = std::make_unique<std::vector<uint32_t> >(indices.size());
+    }
+
+    void Mesh::SetVertices(const std::vector<Vertex> &vertices) {
+        _vertices = std::make_unique<std::vector<Vertex> >(vertices.size());
+    }
+
+}
