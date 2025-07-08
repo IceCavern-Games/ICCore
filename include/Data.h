950371ab41d058699aae92515cc51098d678d4ce
Jordan Hill <jordan.hill@comcast.net>
2025-07-07 19:03:15 -0700

    Add vertex and primitives definitions

---
+++ include/Data.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <Mesh.h>
+
+#include <glm/glm.hpp>
+
+namespace ICCore {
+    struct Vertex {
+        glm::vec3 position;
+        glm::vec3 normal;
+        glm::vec3 color;
+        glm::vec2 uv;
+    };
+}
---
+++ include/Mesh.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Data.h"
+
+#include <memory>
+#include <vector>
+
+namespace ICCore {
+    class Mesh {
+    public:
+        Mesh();
+
+        const std::vector<uint32_t> &GetIndices() const { return *_indices; }
+        const std::vector<Vertex> &GetVertices() const { return *_vertices; }
+
+        void SetVertices(const std::vector<Vertex> &vertices);
+
+        void SetIndices(const std::vector<uint32_t> &indices);
+
+    private:
+        std::unique_ptr<std::vector<Vertex> > _vertices;
+        std::unique_ptr<std::vector<uint32_t> > _indices;
+    };
+}
