077e1911da22ba9ef641c25757199ad059df2b74
Jordan Hill <jordan.hill@comcast.net>
2025-07-07 23:05:26 -0700

    Fix index buffer

--- include/Primitives.h
+++ include/Primitives.h
@@ -62,29 +62,12 @@ namespace ICCore {
         };
 
         const std::vector<uint32_t> indices = {
-            // Front face
-            0, 1, 2,
-            2, 3, 0,
-
-            // Back face
-            4, 5, 6,
-            6, 7, 4,
-
-            // Left face
-            8, 9, 10,
-            10, 11, 8,
-
-            // Right face
-            12, 13, 14,
-            14, 15, 12,
-
-            // Top face
-            16, 17, 18,
-            18, 19, 16,
-
-            // Bottom face
-            20, 21, 22,
-            22, 23, 20
+            0, 1, 2, 3, 4, 5, // Front face
+            6, 7, 8, 9, 10, 11, // Back face
+            12, 13, 14, 15, 16, 17, // Left face
+            18, 19, 20, 21, 22, 23, // Right face
+            24, 25, 26, 27, 28, 29, // Top face
+            30, 31, 32, 33, 34, 35, // Bottom face
         };
 
         Mesh mesh;
